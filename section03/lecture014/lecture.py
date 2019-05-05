fb6bc230a42cbaf08337cb2f11a2bbeaee6bc2e3
agukwt <takuya.stdmailbox#gmail.com>
2019-05-05 19:51:55 +0900

    modify unintended line break.

--- section03/lecture014/lecture.py
+++ section03/lecture014/lecture.py
@@ -2,5 +2,4 @@ print('a is {}'.format('a'))
 print('a is {} {} {}'.format(1, 2, 3))
 print('a is {2} {1} {0}'.format(1, 2, 3))
 print('a is {0} {1} {0}'.format(1, 2, 3))
-print(''
-      'y name is {name} {family}'.format(name='takuya', family='FAMILY'))
+print('My name is {name} {family}'.format(name='takuya', family='FAMILY'))
